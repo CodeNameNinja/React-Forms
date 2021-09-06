53851b31d3475fb91d3680b25ecc99ced476875c
Mitchell <mwhyuen@gmail.com>
2021-09-06 09:35:51 +0200

    feat: add error handling and styling

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,19 +2,28 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
+    setEnteredNameIsValid(true);
   };
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredName.trim().length === 0) {
+      setEnteredNameIsValid(false);
+      return;
+    }
     props.onAdd(enteredName);
     setEnteredName('');
   }
+
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
+        {!enteredNameIsValid && <p className="error-text">Please enter a name</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
